b35c30a3f131aab03d1eed3c3420c102dabffb33
ohtsubo_masakazu <ohtsubo-masakazu@edu.brain.kyutech.jp>
2020-05-07 11:52:17 +0900

    遅延評価のサンプルを作成

--- LazyEvaluationSample/Program.cs
+++ LazyEvaluationSample/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,8 +9,50 @@ namespace LazyEvaluationSample
 {
     class Program
     {
+        /// <summary>
+        /// TextFile1.txtに１と２が含まれているので実際にはTextFile2.txtは読み込まれない。
+        /// もしdataに4が含まれている場合はTextFile2.txtも読み込まれる
+        /// </summary>
+        /// <param name="args"></param>
         static void Main(string[] args)
         {
+            var data = new List<string>() { "1", "2" };
+            
+            foreach(var str in GetLazyEvaluationList())
+            {
+                data.Remove(str);
+                //do task
+                SomeTask(str);
+
+                if (data.Count == 0)
+                {
+                    break;
+                }
+            }
+            Console.WriteLine("completed");
+            Console.ReadLine();
+        }
+
+        static void SomeTask(string str)
+        {
+            Console.WriteLine($"do some task:{str}");
+        }
+
+        public static IEnumerable<string> GetLazyEvaluationList()
+        {
+            var files = new string[] {"TextFile1.txt","TextFile2.txt"};
+            foreach(var file in files)
+            {
+                Console.WriteLine($"open file {file}");
+                using(var sr = new StreamReader(file))
+                {
+                    var str = string.Empty;
+                    while ((str=sr.ReadLine())!=null)
+                    {
+                        yield return str;
+                    }
+                }
+            }
         }
     }
 }
